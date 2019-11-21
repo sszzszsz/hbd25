2c86b78ecf572146bbc650aa0d542a6cba6a5a7c
suzu nagano <sszz12121994@gmail.com>
2019-11-21 12:08:31 +0900

    [fix]ESlintの設定修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,14 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    "semi": [2, "never"],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+      }
+    }],
+    "prettier/prettier": ["error", { "semi": false }]
   }
 }
