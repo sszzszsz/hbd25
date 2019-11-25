1473d70b4424a5acfac354435ef138b128a00dc9
長野鈴 <s.nagano@team.nri-net.com>
2019-11-25 17:31:32 +0900

    [fix/add/del]コンポーネントにしてパーツ作成、不要なコンポーネント削除

---
+++ components/heartMask.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mask_box">
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 512 512"
+      xml:space="preserve"
+      class="mask_svg"
+    >
+      <g>
+        <path
+          class="mask_path"
+          d="M380.63,32.196C302.639,33.698,264.47,88.893,256,139.075c-8.47-50.182-46.638-105.378-124.63-106.879
+    C59.462,30.814,0,86.128,0,187.076c0,129.588,146.582,189.45,246.817,286.25c3.489,3.371,2.668,3.284,2.668,3.284
+    c1.647,2.031,4.014,3.208,6.504,3.208v0.011c0,0,0.006,0,0.011,0c0,0,0.006,0,0.011,0v-0.011c2.489,0,4.856-1.177,6.503-3.208
+    c0,0-0.821,0.086,2.669-3.284C365.418,376.526,512,316.664,512,187.076C512,86.128,452.538,30.814,380.63,32.196z"
+        />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.mask {
+  &_box {
+    width: 95%;
+  }
+  &_path {
+    fill: #fff;
+  }
+}
+</style>
---
+++ components/scrollArrow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="scrollArrow_box">
+    <span class="scrollArrow_item" />
+    <span class="scrollArrow_item" />
+    <span class="scrollArrow_item" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.scrollArrow {
+  &_box {
+    position: absolute;
+    bottom: 15px;
+    left: 15px;
+  }
+  &_item {
+    display: block;
+    width: 10px;
+    height: 10px;
+    border-right: 1px solid #fff;
+    border-bottom: 1px solid #fff;
+    transform: rotate(45deg);
+  }
+}
+</style>
