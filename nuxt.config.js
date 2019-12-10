d332d8adf7b8692a9f9670d83f4456d957e42cff
suzu nagano <sszz12121994@gmail.com>
2019-12-10 16:10:27 +0900

    [add/fix]PWA化

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,26 @@
 import path from 'path'
-// const StylelintPlugin = require('stylelint-webpack-plugin')
 import StylelintPlugin from 'stylelint-webpack-plugin'
+
+// path
+// const baseUrl = process.env.BASE_URL || 'http://localhost:3000'
+const baseDir = process.env.BASE_DIR || '/'
+// const basePath = baseUrl + baseDir
+
+// meta
+const siteName = '好きなところ100'
+const siteDesc = '好きなところ100'
+const maincolor = '#bf4040'
 
 export default {
   mode: 'universal',
+  router: {
+    base: baseDir // ←これも忘れずに入れる！
+  },
   /*
    ** Headers of the page
    */
   head: {
-    title: '好きなところ100',
+    title: siteName,
     titleTemplate: '',
     htmlAttrs: {
       lang: 'ja'
@@ -16,15 +28,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      },
+      { name: 'format-detection', content: 'telephone=no, email=no, address=no' },
+      { hid: 'description', name: 'description', content: siteDesc },
       {
         hid: 'msapplication-TileColor',
         name: 'msapplication-TileColor',
-        content: '#bf4040'
+        content: maincolor
       },
       {
         hid: 'theme-color',
@@ -33,15 +42,107 @@ export default {
       }
     ],
     link: [
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      },
+      // ogp関連
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'mask-icon', href: 'safari-pinned-tab.svg', color: '#bf4040' }
+      { rel: 'mask-icon', href: 'safari-pinned-tab.svg', color: maincolor },
+      // ogp関連
+      { hid: 'og:site_name', property: 'og:site_name', content: siteName },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://hbd25.netlify.com/' },
+      { hid: 'og:title', property: 'og:title', content: siteName },
+      { hid: 'og:description', property: 'og:description', content: siteDesc },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      // pwa iOS
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      // iOS splashscreens
+      {
+        href: '/splashscreens/iphone5_splash.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/iphone6_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/iphoneplus_splash.png',
+        media:
+          '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/iphonex_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/iphonexr_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/iphonexsmax_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/ipad_splash.png',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/ipadpro1_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/ipadpro3_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)',
+        rel: 'apple-touch-startup-image'
+      },
+      {
+        href: '/splashscreens/ipadpro2_splash.png',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
+        rel: 'apple-touch-startup-image'
+      }
     ]
+  },
+  /*
+   ** manifest.json
+   */
+  manifest: {
+    name: siteName,
+    short_name: siteName,
+    description: siteDesc,
+    lang: 'ja',
+    theme_color: maincolor,
+    background_color: maincolor,
+    display: 'standalone',
+    scope: '/',
+    orientation: 'portrait',
+    icons: [
+      {
+        src: '/icon-1024.png',
+        sizes: '1024x1024',
+        type: 'image/png'
+      }
+    ]
+  },
+  workbox: {
+    dev: true // 開発環境でもPWA
   },
   /*
    ** Customize the progress-bar color
@@ -78,7 +179,7 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa',
+    ['@nuxtjs/pwa', { icon: false }],
     '@nuxtjs/style-resources',
     'nuxt-user-agent',
     'nuxt-webfontloader'
@@ -91,6 +192,9 @@ export default {
       families: ['Sawarabi+Gothic', 'Libre+Baskerville']
     }
   },
+  /*
+   ** ページ遷移アニメーション
+   */
   pageTransition: {
     name: 'fade-up',
     mode: 'out-in',
