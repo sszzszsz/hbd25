655483be57fa67f91de8e78cf0cda5b01d3d708b
Suzu Nagano <sszz12121994@gmail.com>
2020-03-19 19:07:30 +0900

    [fix]SVGでなみなみの追加

--- pages/point/index.vue
+++ pages/point/index.vue
@@ -2,6 +2,115 @@
   <main class="main">
     <mousePointer />
     <div class="cont">
+      <div class="bg_cont">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="1370.718"
+          height="1225.018"
+          viewBox="0 0 1370.718 1225.018"
+          class="svg_pale"
+        >
+          <defs>
+            <filter
+              id="a"
+              x="0"
+              y="0"
+              width="1370.718"
+              height="1225.018"
+              filterUnits="userSpaceOnUse"
+            >
+              <feOffset dy="3" input="SourceAlpha" />
+              <feGaussianBlur stdDeviation="3" result="b" />
+              <feFlood flood-color="#2c2c2c" flood-opacity="0.161" />
+              <feComposite operator="in" in2="b" />
+              <feComposite in="SourceGraphic" />
+            </filter>
+          </defs>
+          <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#a)">
+            <path
+              d="M1081.773,4838.157c115.7-37.7,206.7,63.7,239.1-194.187s214.871-133.158,235.188-310.518,40.635-276.216,196.4-302.384S1912.51,3869.878,1933,3808.153s138.694-111.45,138.694-111.45h0V5049.42H864.673S966.073,4875.857,1081.773,4838.157Z"
+              transform="translate(-3687.7 2077.69) rotate(-90)"
+              fill="#e5cece"
+            >
+              <animate
+                attributeName="d"
+                values="
+M1081.773,4838.157c115.7-37.7,206.7,63.7,239.1-194.187s214.871-133.158,235.188-310.518,40.635-276.216,196.4-302.384S1912.51,3869.878,1933,3808.153s138.694-111.45,138.694-111.45h0V5049.42H864.673S966.073,4875.857,1081.773,4838.157Z;
+            M844.666,4938.96c20-61,138.646-15.322,327.952-120.24s54.144-396.4,191.932-451.835,311.886-3.713,323.028-200.5,33.417-237.628,204.211-271.044,179.9-198.638,179.9-198.638h0V5049.42H864.673S824.666,4999.96,844.666,4938.96Z;
+            M1081.773,4838.157c115.7-37.7,206.7,63.7,239.1-194.187s214.871-133.158,235.188-310.518,40.635-276.216,196.4-302.384S1912.51,3869.878,1933,3808.153s138.694-111.45,138.694-111.45h0V5049.42H864.673S966.073,4875.857,1081.773,4838.157Z"
+                dur="15s"
+                repeatCount="indefinite"
+              />
+            </path>
+          </g>
+        </svg>
+
+        <!-- <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="1370.718"
+          height="1250.625"
+          viewBox="0 0 1370.718 1250.625"
+        >
+          <defs>
+            <filter
+              id="a"
+              x="0"
+              y="0"
+              width="1370.718"
+              height="1250.625"
+              filterUnits="userSpaceOnUse"
+            >
+              <feOffset dy="3" input="SourceAlpha" />
+              <feGaussianBlur stdDeviation="3" result="b" />
+              <feFlood flood-color="#2c2c2c" flood-opacity="0.161" />
+              <feComposite operator="in" in2="b" />
+              <feComposite in="SourceGraphic" />
+            </filter>
+          </defs>
+          <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#a)">
+            <path
+              d="M844.666,4938.96c20-61,138.646-15.322,327.952-120.24s54.144-396.4,191.932-451.835,311.886-3.713,323.028-200.5,33.417-237.628,204.211-271.044,179.9-198.638,179.9-198.638h0V5049.42H864.673S824.666,4999.96,844.666,4938.96Z"
+              transform="translate(-3687.7 2077.69) rotate(-90)"
+              fill="#e5cece"
+            />
+          </g>
+        </svg> -->
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="693.776"
+          height="552.406"
+          viewBox="0 0 693.776 552.406"
+          class="svg_dark"
+        >
+          <defs>
+            <filter
+              id="a"
+              x="0"
+              y="0"
+              width="693.776"
+              height="552.406"
+              filterUnits="userSpaceOnUse"
+            >
+              <feOffset dy="3" input="SourceAlpha" />
+              <feGaussianBlur stdDeviation="3" result="b" />
+              <feFlood flood-color="#570000" flood-opacity="0.349" />
+              <feComposite operator="in" in2="b" />
+              <feComposite in="SourceGraphic" />
+            </filter>
+          </defs>
+          <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#a)">
+            <path
+              d="M1237.865,4708.665s-13.476-145.563,89.583-175.922,233.281-51.929,222.1-223.694,222.148-276.16,222.148-276.16-1.36,675.385,0,675.776Z"
+              transform="translate(-4023.89 1777.69) rotate(-90)"
+              fill="#d5a9a9"
+            />
+          </g>
+        </svg>
+      </div>
       <div
         :class="{
           'is-ready': this.$store.state.global.loadingEnd === true
@@ -165,8 +274,10 @@ export default Vue.extend({
   &:after {
     content: '';
     display: block;
+    position: fixed;
+    z-index: 5;
     width: 100%;
-    border: 10px solid;
+    border-top: 10px solid;
     left: 0;
     border-color: var(--main-color);
   }
@@ -174,9 +285,10 @@ export default Vue.extend({
     top: 0;
   }
   &:after {
-    bottom: 0;
+    top: calc(100vh - 10px);
   }
 }
+
 .cont {
   position: relative;
   background: url('~assets/img/bg_paper.jpg');
@@ -184,15 +296,55 @@ export default Vue.extend({
   -ms-overflow-style: none; /* IE, Edge 対応 */
   scrollbar-width: none; /* Firefox 対応 */
   height: 100%;
+  min-height: 100vh;
   z-index: 2;
+  padding: 10px;
   &::-webkit-scrollbar {
     display: none;
+  }
+  &:before,
+  &:after {
+    content: '';
+    display: block;
+    position: fixed;
+    z-index: 5;
+    width: 10px;
+    height: 100vh;
+    border-left: 10px solid;
+    top: 0;
+    border-color: var(--main-color);
+  }
+  &:before {
+    left: 0;
+  }
+  &:after {
+    right: 0;
+  }
+}
+.bg_cont {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 3;
+
+  svg {
+    position: fixed;
+    top: 0;
+    right: -10px;
+    z-index: 4;
+    width: 100%;
+    height: auto;
+
+    &.svg_dark {
+      width: 50%;
+    }
   }
 }
 .inr {
   padding-left: pcvw(150px);
   position: relative;
-  z-index: 2;
+  z-index: 5;
   &:after {
     content: '';
     display: block;
